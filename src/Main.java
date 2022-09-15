45e7839bfcbf9e1caffce3a2c596657faa6b14b8
minik <keggbj@mail.uc.edu>
2022-09-15 17:08:15 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+     double Winter = 25_000;
+     double Spring = 20_000;
+     double Summer = 15_000;
+     double Autumn = 20_000;
+     int totalCost = (int) (Winter + Spring + Summer + Autumn);
+
+     System.out.println("The total yearly maintenance cost is: " + totalCost);
+
     }
 }
